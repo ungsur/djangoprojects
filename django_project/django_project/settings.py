86ab7b00888352c01d7875b870cd44cce2a90dae
Richard Ungsunan <ungsunan@gmail.com>
2019-10-23 18:11:54 -0400

    Django tutorial lesson 9 complete

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
